3b423100b5915e1aca4edbf88a666d20c6f85cd9
Metaxal <laurent.orseau@gmail.com>
2014-04-09 17:23:54 +0200

    Sélection quadratique au lieu de cubique...

--- TrafficJamAGPlayerEns.java
+++ TrafficJamAGPlayerEns.java
@@ -5,17 +5,17 @@ public class TrafficJamAGPlayerEns extends TrafficJamAGPlayer {
 	protected TrafficJamAGIndividuEns[] _individus2;
 	protected double _bestFitness;
 	protected double _avgFitness;
-	
+
 	protected TrafficJamAGIndividuEns _best;
 	protected int _numCoup;
 
 	public TrafficJamAGPlayerEns() {
 		super();
 	}
-	
+
 	public void initLearning() {
 		_best = null;
-		
+
 		genese();
 	}
 
@@ -32,11 +32,11 @@ public class TrafficJamAGPlayerEns extends TrafficJamAGPlayer {
 		int nbi = nbIndividus();
 		_individus = new TrafficJamAGIndividuEns[nbi];
 		_individus2 = new TrafficJamAGIndividuEns[nbi];
-		
+
 		for(int i = 0; i < nbi; i++)
 			_individus[i] = new TrafficJamAGIndividuEns(this);
 	}
-	
+
 	public void learnAG(){
 		System.out.println("nb Generation " + nbGenerations() + " stop = " + stop());
 		for (int i = 0; i < nbGenerations() && !stop(); i++ ) {
@@ -53,7 +53,7 @@ public class TrafficJamAGPlayerEns extends TrafficJamAGPlayer {
 		System.out.println("Best fitness = " + _bestFitness);
 		System.out.println("Avg  fitness = " + _avgFitness);
 	}
-	
+
 	public void uneGeneration() {
 		// calcul de la performance
 		_avgFitness = 0.;
@@ -65,20 +65,22 @@ public class TrafficJamAGPlayerEns extends TrafficJamAGPlayer {
 
 		// tri du tableau
 		Arrays.sort(_individus);
-		
+
 		_best = _individus[0];
 		_bestFitness = _best.fitness();
-		
+
 		// Sélection, croisement, mutation
 		// puis remplissage des cases vides avec de nouveux inidividus aléatoires
 		_individus2[0] = _individus[0];
 		int i2;
 		for(i2 = 1; i2 < nbIndividus()*tauxSelection(); i2++) {
 			double x1 = Math.random(); // strictement < 1.0
-			x1 = x1*x1*x1; // pour s'aprocher de 0, donc du meilleur
+			//x1 = x1*x1*x1; // pour s'aprocher de 0, donc du meilleur
+			x1 = x1*x1; // pour s'aprocher de 0, donc du meilleur
 			double x2 = Math.random();
 			x2 = x2*x2*x2;
-			int n1 = (int)Math.floor(x1*nbIndividus()); // on évite le best ?
+			x2 = x2*x2;
+			int n1 = (int)Math.floor(x1*nbIndividus());
 			int n2 = (int)Math.floor(x2*nbIndividus());
 			if(Math.random() < tauxCroisement())
 				_individus2[i2] = _individus[n1].croisement(_individus[n2]);
@@ -87,17 +89,17 @@ public class TrafficJamAGPlayerEns extends TrafficJamAGPlayer {
 
 			_individus2[i2].mutation(tauxMutation());
 		}
-		
+
 		// on comble avec de l'aléatoire
-		for(; i2 < nbIndividus(); i2++) 
+		for(; i2 < nbIndividus(); i2++)
 			_individus2[i2] = new TrafficJamAGIndividuEns(this);
-		
+
 	}
-	
+
 	public void nouvelleGeneration() {
 		_individus = _individus2;
 	}
-	
+
 	public int joue() {
 		if(_best != null)
 			return _best.gene(_numCoup++); // séquence des coups du meilleur individu/solution
